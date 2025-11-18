c549d09475d16a33d0acf774a18fb8044e0c2144
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2025-11-18 12:09:56 -0500

    Updated GardenService

--- Services/GardenService.cs
+++ Services/GardenService.cs
@@ -53,9 +53,17 @@ namespace GardenHub.Services
             var existingGarden = await _context.Gardens.FindAsync(id);
             if (existingGarden != null)
             {
-                garden.StartDate = DateTime.SpecifyKind(garden.StartDate, DateTimeKind.Utc);
-                garden.EndDate = DateTime.SpecifyKind(garden.EndDate, DateTimeKind.Utc);
-                _context.Update(garden);
+                // Update properties of the tracked entity
+                existingGarden.GardenName = garden.GardenName;
+                existingGarden.GardenDescription = garden.GardenDescription;
+                existingGarden.GardenLocation = garden.GardenLocation;
+                existingGarden.Type = garden.Type;
+                existingGarden.GardenGrowMethod = garden.GardenGrowMethod;
+                existingGarden.StartDate = DateTime.SpecifyKind(garden.StartDate, DateTimeKind.Utc);
+                existingGarden.EndDate = DateTime.SpecifyKind(garden.EndDate, DateTimeKind.Utc);
+                existingGarden.UserId = garden.UserId;
+                
+                // No need to call Update() - EF will detect changes automatically
                 await _context.SaveChangesAsync();
             }
         }

6d695219b0c9739e0078d2b142a293696cdd79f9
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2025-11-18 07:34:32 -0500

    Add project files.

---
+++ Models/AppUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace GardenHub.Models
+{
+    public class AppUser : IdentityUser
+    {
+        [Required]
+        [StringLength(30, ErrorMessage = "First name must be between {2} and {0} characters long.", MinimumLength = 2)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(30, ErrorMessage = "Last name must be between {2} and {0} characters long.", MinimumLength = 2)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; } = string.Empty;
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
